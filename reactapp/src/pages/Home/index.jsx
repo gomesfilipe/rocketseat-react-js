b31d94c364b504ea024bbac079be3633719f5889
gomesfilipe <filipejvmf@gmail.com>
2022-05-31 09:49:07 -0300

    .

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './style.css'
 import {Card} from '../../components/Card'
 
 export function Home() {
   const [studentName, setStudentName] = useState('')
   const [students, setStudents] = useState([])
+  const [user, setUser] = useState({name: '', avatar: ''})
 
   function handle_add_student() {
     const newStudent = {
@@ -19,9 +20,28 @@ export function Home() {
     setStudents(previousState => [...previousState, newStudent])
   }
 
+  // Executa sempre que a página é renderizada e quando alguma variável do vetor de depêndencias é alterada.
+  useEffect(() => {
+    fetch('https://api.github.com/users/gomesfilipe')
+      .then(response => response.json())
+      .then(data => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url
+        })
+      })
+  }, []) // Vetor de dependências.
+
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil." />
+        </div>
+      </header>
+      
       <input 
         type="text" 
         placeholder="Digite o nome..." 
@@ -32,7 +52,7 @@ export function Home() {
       </button>
 
       {
-        students.map((student, i) => <Card name={student.name} time={student.time} key={i}/>)
+        students.map(student => <Card name={student.name} time={student.time} key={student.time}/>)
       }
     </div>
   )
